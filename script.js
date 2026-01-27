c7ad66ae197fdb8d234eb251026be1d54391d83c
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-01-27 19:42:11 +0530

    Completed the task with simplest logic

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-let todos = [
-    { id: 1, task: "Morning walk" },
-    { id: 2, task: "Attend classes" }
-];
-
+let todos = [`Morning Cardio`, `Go to College`]
 let list = document.getElementById("todoList");
 let input = document.getElementById("todoInput");
 let button = document.getElementById("addBtn");
 
 function showTodos() {
-    list.innerHTML = "";
-
-    for (let i = 0; i < todos.length; i++) {
-        let li = document.createElement("li");
-        li.className = "todo-item";
-
-        li.innerHTML =
-            "<span>" + todos[i].task + "</span>" +
-            "<span class='todo-id'>ID: " + todos[i].id + "</span>";
-
-        list.appendChild(li);
-    }
-}
-
-button.onclick = function () {
-    let text = input.value;
-
-    if (text === "") {
-        return;
-    }
-
-    let newTodo = {
-        id: todos.length + 1,
-        task: text
+    addBtn.onclick = function() {
+        let task = input.value;
+        todos.push(task);
+        showTodos();
     };
 
-    todos.push(newTodo);
-    input.value = "";
-
-    showTodos();
-};
-
+    list.innerHTML = todos.join(`<br>`);
+}
 showTodos();
