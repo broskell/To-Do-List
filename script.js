9a7e8a8909d59ce062df1972b69940cd30013d23
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-02-06 19:09:15 +0530

    Completed Create and Read in CRUD operations

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-let todos = [`Morning Cardio`, `Go to College`]
+let todos = [
+    {
+        id : 1,
+        todo: 'Morning Cardio'
+    },
+    {
+        id : 2,
+        todo: 'Go to College'
+    }
+]
 let list = document.getElementById("todoList");
 let input = document.getElementById("todoInput");
 let button = document.getElementById("addBtn");
 
 function showTodos() {
-    addBtn.onclick = function() {
-        let task = input.value;
-        todos.push(task);
-        showTodos();
-    };
-
-    list.innerHTML = todos.join(`<br>`);
+    let html = '';
+    for(let i = 0; i < todos.length; i++) {
+        html += todos[i].id + '. ' + todos[i].todo + '<br>';
+    }
+    list.innerHTML = html;
+    input.value = '';
 }
+
+button.onclick = function() {
+    let task = input.value;
+    if (input.value === '') {
+        return;
+    }
+    todos.push({
+        id : todos.length + 1,
+        todo: task
+    });
+    showTodos();
+}
+
 showTodos();
