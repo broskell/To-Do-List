0b02a6ec66d227e2c0d3f880f0bc3e2bd15d3c7c
Kellampalli Saathvik <saathvik.kp@gmail.com>
2026-02-12 12:03:13 +0530

    refactor: update button functionality and styles for todo list

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
 let todos = [
-    {
-        id : 1,
-        todo: 'Morning Cardio'
-    },
-    {
-        id : 2,
-        todo: 'Go to College'
-    }
-]
-let list = document.getElementById("todoList");
-let input = document.getElementById("todoInput");
-let button = document.getElementById("addBtn");
-let clear = document.getElementById("clearBtn");
+    {id: 1, todo: `Morning Cardio`},
+    {id: 2, todo: `Go to college`}
+];
 
-function showTodos() {
-    let html = '';
-    for(let i = 0; i < todos.length; i++) {
+input = document.getElementById(`todoInput`);
+todoList = document.getElementById(`todoList`);
+addBtn = document.getElementById(`addBtn`);
+clearBtn = document.getElementById(`clearBtn`);
+
+function showTodo() {
+    let html = ``;
+    for (let i = 0; i < todos.length; i++) {
         html += `<div>
-                    <span onclick="updateTodo(${todos[i].id})">${todos[i].id}. ${todos[i].todo}</span>
-                    <button class="delete-btn" onclick="deleteBtn(${todos[i].id})">Delete</button>
-                </div>`;
+                    <span onclick="updateBtn(${todos[i].id})">${todos[i].id}. ${todos[i].todo}</span>
+                    <button class="done-btn" onclick="doneBtn(${todos[i].id})">Done</button>
+                    <br>
+                </div>`
     }
-    list.innerHTML = html;
+    todoList.innerHTML = html;
+}
+
+addBtn.onclick = function() {
+    newTodo = input.value;
+    todos.push({id: todos.length + 1, todo: newTodo});
+
     input.value = '';
+    showTodo();
 }
 
-button.onclick = function() {
-    let task = input.value;
-    if (input.value === '') {
-        return;
-    }
-    todos.push({
-        id : todos.length + 1,
-        todo: task
-    });
-    showTodos();
-}
-
-clear.onclick = function() {
+clearBtn.onclick = function() {
     todos = [];
-    showTodos();
+    showTodo();
 }
 
-function deleteBtn(id) {
-    todos = todos.filter(function(item) { 
-        return item.id != id 
-    });
-    showTodos();    
+function doneBtn(id) {
+    todos = todos.filter(function(item){
+        return item.id !== id;
+    })
+    showTodo();
 }
 
-function updateTodo(id) {
-    for(let i = 0; i < todos.length; i++) {
-        if(todos[i].id === id) {
-            todos[i].todo = prompt("Edit task:", todos[i].todo);
-            if(todos[i].todo === null || todos[i].todo.trim() === '') {
-                todos[i].todo = 'Untitled Task';
+function updateBtn(id) {
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos[i].todo = prompt(`Enter a value`, todos[i].todo);
+            if (todos[i].todo === null || todos[i].todo === "") {
+                todos[i].todo = `Untitled`;
             }
-            showTodos();
         }
     }
+    showTodo();
 }
 
-showTodos();
+showTodo();
